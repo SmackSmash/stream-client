07e4844c237249afe42b5ca7f43f8b3ed5de9a54
Dan Moffat <danmoffat@gmail.com>
2019-07-01 22:26:45 +0100

    Added OAuth authentication and login/logout functionality and hooked auth status into redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = props => {
   return (
     <div className="ui secondary pointing menu">
-      <div className="right menu">Header</div>
+      <Link to="/">Streamer</Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+        <GoogleAuth />
+      </div>
     </div>
   );
 };
