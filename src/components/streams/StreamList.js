32415363fc74c78d1eb1a55014a9e193c44c29b1
Dan Moffat <danmoffat@gmail.com>
2019-07-03 18:05:54 +0100

    Switched BrowserRouter to a plain Router with custom history object

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -44,9 +44,11 @@ class StreamList extends Component {
   renderCreate = () => {
     if (this.props.isSignedIn) {
       return (
-        <Link to="/streams/new" className="ui button primary">
-          Create Stream
-        </Link>
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">
+            Create Stream
+          </Link>
+        </div>
       );
     }
   };
